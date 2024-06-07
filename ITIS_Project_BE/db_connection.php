f5a8ca7719e77aa3da3a566b96919e22dbf1e4b0
DulanjaliSankapaliKoswatte <dulanjali.koswatte@gmail.com>
2024-06-08 04:04:47 +1000

    Database user as appuser

--- ITIS_Project_BE/db_connection.php
+++ ITIS_Project_BE/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 try {
     // Database connection
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=library_managment', 'root', 'Abcd@1234');
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=library_managment', 'appuser', 'Abcd@1234');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     error_log("Database connection failed: " . $e->getMessage());
