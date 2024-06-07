d3ec81264e43da3a6021e1806489e08e1150c5a3
DulanjaliSankapaliKoswatte <dulanjali.koswatte@gmail.com>
2024-06-08 03:51:48 +1000

    Database pw as root

--- ITIS_Project_BE/db_connection.php
+++ ITIS_Project_BE/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 try {
     // Database connection
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=library_managment', 'root', '');
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=library_managment', 'root', 'root');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     error_log("Database connection failed: " . $e->getMessage());
