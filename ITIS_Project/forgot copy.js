bc10ce8746d35e72b07a0f94a5fc0dde813776ca
DulanjaliSankapaliKoswatte <dulanjali.koswatte@gmail.com>
2024-06-09 04:57:04 +1000

    Reset UI 8

---
+++ ITIS_Project/forgot copy.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('forgot-form');
+    if (form){
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const email = document.getElementById('email').value;
+       
+        fetch('https://itis-group11.com/Group11_LibryMangmnt/ITIS_Project_BE/forgot.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: email })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Password reset instructions have been sent to your email.');
+                window.location.href = 'reset.html';
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again.');
+        });
+    });
+}
+});
