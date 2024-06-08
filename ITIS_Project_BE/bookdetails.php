06109dd7db713f5c7da9d196bbfbe9d1c11b70bf
DulanjaliSankapaliKoswatte <dulanjali.koswatte@gmail.com>
2024-06-08 20:08:04 +1000

    bookdetails.php 4

--- ITIS_Project_BE/bookdetails.php
+++ ITIS_Project_BE/bookdetails.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 
 require 'validate_token.php';
 require 'db_connection.php'; // Database connection
