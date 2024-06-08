10769dbad363c610ca2607f71a002f98ce80e795
DulanjaliSankapaliKoswatte <dulanjali.koswatte@gmail.com>
2024-06-09 06:06:40 +1000

    Password strength

--- ITIS_Project/reset.js
+++ ITIS_Project/reset.js
@@ -48,17 +48,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (newPassword !== confirmPassword) {
             confirmPasswordInput.setCustomValidity("Passwords do not match");
-            alert("Passwords do not match");
-        }  else if (checkPasswordStrength(newPassword) < 4) {
-            alert('Please ensure the password is strong enough (includes uppercase, lowercase, numbers, and symbols).');
-            return;
-        }else {
+        } else {
             confirmPasswordInput.setCustomValidity("");
         }
     });
 
     // Check if the password is strong
-   
+        if (checkPasswordStrength(passwordInput.value) < 4) {
+            alert('Please ensure the password is strong enough (includes uppercase, lowercase, numbers, and symbols).');
+            return;
+        }
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
